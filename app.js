ed5d07ae45a2f1be9c1c271e5ec07446837e8d5a
Morteza Aubi <mortezaaubi96@gmail.com>
2026-02-20 20:20:21 +0330

    commit test files

---
+++ app.js
@@ -0,0 +1,15 @@
+const isLogin = (user) => {
+  return true;
+};
+
+const register = (user) => {
+  return true;
+};
+
+const getMe = (user) => {
+  return "user";
+};
+
+const addCourse = (course) => {
+  return true;
+};
---
+++ funcs.js
@@ -0,0 +1,17 @@
+// Test Codes
+
+const likePost = () => {
+  return true;
+};
+
+const disslikePost = () => {
+  return false;
+};
+
+const getMe = (user) => {
+  return user.name;
+};
+
+const setComment = (comment) => {
+  return comment.body;
+};
